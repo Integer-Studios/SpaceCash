24fe643f686ee4920df21feff2c695c63d0a72a0
hardcorebadger <trefethen@Jakes-MacBook-Pro-2.local>
2018-02-01 12:27:56 -0800

    net transforms

---
+++ Assets/LilSumn/Scripts/PlayerMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.FirstPerson; 
+
+public class PlayerMove : NetworkBehaviour {
+
+	public float Speed;
+
+	void Start() {
+		transform.parent = FindObjectOfType<Ship> ().transform;
+		if (!isLocalPlayer)
+			GetComponentInChildren<Camera> ().enabled = false;
+	}
+	
+	void Update() {
+		if (!isLocalPlayer)
+			return;
+		transform.Translate (new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical")) * Time.deltaTime * Speed);
+	}
+}
---
+++ Assets/LilSumn/Scripts/Ship.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Ship : NetworkBehaviour {
+
+	private void Start() {
+		if (NetworkServer.active)
+			StartServer ();
+		if (NetworkClient.active)
+			StartClient ();
+	}
+
+	private void StartServer() {
+
+	}
+
+	private void StartClient() {
+
+	}
+
+	private void Update() {
+		if (NetworkServer.active)
+			UpdateServer ();
+		if (NetworkClient.active)
+			UpdateClient ();
+	}
+
+	private void UpdateServer() {
+		transform.Translate (Vector3.forward * Time.deltaTime * 3f);
+	}
+
+	private void UpdateClient() {
+		
+	}
+	
+}
