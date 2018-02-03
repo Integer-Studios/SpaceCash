759029c4a94bcf7f7256444d118ad00cf4875119
hardcorebadger <trefethen@Jakes-MacBook-Pro-2.local>
2018-02-02 16:34:13 -0800

    iuw

--- Assets/LilSumn/Scripts/Player.cs
+++ Assets/LilSumn/Scripts/Player.cs
@@ -1,26 +1,52 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
 using UnityStandardAssets.Characters.FirstPerson; 
+using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : NetworkBehaviour {
 
 	private Ship _ship;
+	private Animator _animator;
+
+	[SyncVar]
+	private int _walkSpeed;
 
 	void Start() {
 		
 		_ship = FindObjectOfType<Ship> ();
+		_animator = GetComponentInChildren<Animator> ();
 		transform.parent = _ship.transform;
 
 		if (!isLocalPlayer) {
 			GetComponentInChildren<Camera> ().enabled = false;
 			GetComponent<CharacterController> ().enabled = false;
-			GetComponent<AudioListener> ().enabled = false;
+			GetComponentInChildren<AudioListener> ().enabled = false;
 		}
 	}
 	
 	void Update() {
-		if (!isLocalPlayer)
+		_animator.SetFloat ("Speed_f", ((float)_walkSpeed)/2f);
+
+		if (!isLocalPlayer) {
 			return;
+		}
+		
+		int i = 0;
+		if (Mathf.Abs(CrossPlatformInputManager.GetAxis ("Vertical")) > 0.5f)
+			i = 1;
+		if (Input.GetKey (KeyCode.LeftShift))
+			i = 2;
+
+		if (_walkSpeed != i) {
+			_walkSpeed = i;
+			CmdSetWalkSpeed (_walkSpeed);
+		}
+		
 	}
 
+	[Command]
+	void CmdSetWalkSpeed(int f) {
+		if (!isLocalPlayer)
+			_walkSpeed = f;
+	}
 }
